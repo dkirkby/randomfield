8ecf8bd715b590fb2e57451428c885e8a6c82fc3
David Kirkby <dkirkby@uci.edu>
2015-08-12 12:14:17 -0700

    Clean up cython experiments

--- randomfield/crandom.c
+++
@@ -1,5 +0,0 @@
-#include "crandom.h"
-
-int add_one(int value) {
-    return value + 1;
-}
--- randomfield/random.pyx
+++
@@ -1,7 +0,0 @@
-cdef extern from "crandom.h":
-    int add_one(int value)
-
-def generate_normal_deviates(int value):
-    print 'generating via crandom...'
-    result = add_one(value)
-    return result
