Add Gaussian variance test (and fix normalization)

randomfield/power.py
@@ -81,7 +81,7 @@ def tabulate_sigmas(data, power, spacing, packed=True):
 
     # Build an interpolater of sigma(|k|) that is linear in log10(|k|).
     log10_k = np.log10(power['k'])
-    sigma = N3 * np.sqrt(power['Pk'] / (2 * Vbox))
+    sigma = N3**0.5 * np.sqrt(power['Pk'] / (2 * Vbox))
     interpolator = scipy.interpolate.interp1d(
         log10_k, sigma, kind='linear', copy=False,
         bounds_error=False, fill_value=0)

randomfield/tests/test_generate.py
@@ -5,15 +5,57 @@ from __future__ import print_function, division
 from astropy.tests.helper import pytest
 from ..generate import *
 import numpy as np
+from scipy.special import erf
 
 from ..power import load_default_power
 
+spacing = 2.5
+nxyz = 64
+seed = 123
 
 def test_generate():
-    spacing = 2.5
-    nx, ny, nz = 64, 64, 64
     power = load_default_power()
-    data = generate(nx, ny, nz, spacing, power, seed=123)
-    assert data.shape == (nx, ny, nz)
+    data = generate(nxyz, nxyz, nxyz, spacing, power, seed=seed)
+    assert data.shape == (nxyz, nxyz, nxyz)
     assert data.dtype == np.float32
     assert abs(np.mean(data)) < 1e-3
+
+
+def test_gaussian_variance():
+    """
+    Tabulate a Gaussian power spectrum::
+
+        P(k) = P0*exp(-(k*sigma)**2/2)
+
+    corresponding to a real-space Gaussian smoothing of white noise, with
+    correlation function::
+
+        xi(r) = P0*exp(-r**2/(2*sigma**2))/(2*pi)**(3/2)/sigma**3
+
+    The corresponding variance integrated over a grid with
+    kmin <= kx,ky,kz <= kmax is::
+
+        P0/(2*pi)**(3/2)/sigma**3 *
+            (erf(kmax*sigma/sqrt(2))**3 - erf(kmin*sigma/sqrt(2))**3)
+    """
+    kmin = (2 * np.pi) / (spacing * nxyz)
+    kmax = np.pi / spacing
+    sigma = 2.5 * spacing
+    P0 = 1.23
+
+    calculated_var = P0 / (2 * np.pi)**1.5 / sigma**3 * (
+        erf(kmax * sigma / np.sqrt(2))**3 -
+        erf(kmin * sigma / np.sqrt(2))**3)
+
+    power = np.empty(100, dtype=[('k', float), ('Pk', float)])
+    power['k'] = np.linspace(kmin, np.sqrt(3)*kmax, len(power))
+    power['Pk'] = P0 * np.exp(-0.5 * (power['k'] * sigma)**2)
+
+    ntrials = 10
+    measured_var = 0
+    for trial in range(ntrials):
+        data = generate(nxyz, nxyz, nxyz, spacing, power, seed=seed + trial)
+        measured_var += np.var(data)
+    measured_var /= ntrials
+
+    assert abs(measured_var - calculated_var) < 0.01 * calculated_var

randomfield/tests/test_power.py
@@ -122,7 +122,7 @@ def test_tabulate_sigmas():
                     if ix == 0 and iy == 0 and iz == 0:
                         continue
                     k = np.sqrt(ksq)
-                    sigma = N3 * np.sqrt(Pk(k)/(2 * Vbox))
+                    sigma = N3**0.5 * np.sqrt(Pk(k)/(2 * Vbox))
                     # Match with a loose tolerance since we are using a
                     # different interpolation scheme to calculate sigma here.
                     assert abs(plan.data_in[ix, iy, iz] - sigma) < 1e-3 * sigma