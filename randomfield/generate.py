ebb63dd428a8593f6860ffbca38f86183b24bf18
David Kirkby <dkirkby@uci.edu>
2015-08-18 22:52:02 -0700

    Add method to calculate the Newtonian potential

--- randomfield/generate.py
+++ randomfield/generate.py
@@ -9,6 +9,9 @@ import random
 import numpy as np
 import scipy.interpolate
 
+import astropy.units as u
+import astropy.constants
+
 import transform
 import powertools
 import cosmotools
@@ -48,7 +51,7 @@ class Generator(object):
     verbose: bool, optional.
         Print a summary of this generator's parameters.
     """
-    def __init__(self, nx, ny, nz, grid_spacing_Mpc_h,
+    def __init__(self, nx, ny, nz, grid_spacing_Mpc_h, num_plot_sections=4,
                  cosmology=None, power=None, verbose=False):
         self.plan_c2r = transform.Plan(
             shape=(nx, ny, nz), dtype_in=np.complex64,
@@ -58,6 +61,13 @@ class Generator(object):
         self.grid_spacing_Mpc_h = grid_spacing_Mpc_h
         self.k_min, self.k_max = powertools.get_k_bounds(
             self.plan_c2r.data_in, grid_spacing_Mpc_h, packed=True)
+        self.potential = None
+
+        if nz % num_plot_sections != 0:
+            raise ValueError(
+                'Z-axis does not evenly divided into {0} plot sections.'
+                .format(num_plot_sections))
+        self.num_plot_sections = num_plot_sections
 
         if cosmology is None:
             self.cosmology = cosmotools.create_cosmology()
@@ -102,6 +112,7 @@ class Generator(object):
                   .format(self.x_fov * self.y_fov))
 
     def generate_delta_field(self, smoothing_length_Mpc_h=0., seed=None,
+                             save_potential=True,
                              show_plot=False, save_plot_name=None):
         """
         Generate a delta-field realization.
@@ -115,6 +126,10 @@ class Generator(object):
             Random number seed to use. Specifying an explicit seed enables you
             to generate a reproducible delta field.  If no seed is specified,
             a randomized seed will be used.
+        save_potential: bool, optional
+            Save the k-space field delta(kx,ky,kz) / k**2 so that it can be
+            used for later calculations of the lensing potential or the
+            bulk velocity vector field.
         """
         powertools.fill_with_log10k(
             self.plan_c2r.data_in, spacing=self.grid_spacing_Mpc_h, packed=True)
@@ -125,6 +140,23 @@ class Generator(object):
             spacing=self.grid_spacing_Mpc_h, packed=True)
         random.randomize(self.plan_c2r.data_in, seed=seed)
         transform.symmetrize(self.plan_c2r.data_in, packed=True)
+        if save_potential:
+            # Fill self.potential with values of k**2.
+            self.potential = np.empty_like(self.plan_c2r.data_in)
+            self.potential.imag = 0.
+            kx2_grid, ky2_grid, kz2_grid = powertools.create_ksq_grids(
+                self.potential, spacing=self.grid_spacing_Mpc_h, packed=True)
+            np.add(kx2_grid, ky2_grid, out=self.potential.real)
+            self.potential.real += kz2_grid
+            # Replace k**2 with 1 / k**2 except at k=0.
+            old_settings = np.seterr(divide='ignore')
+            np.reciprocal(self.potential.real, out=self.potential.real)
+            np.seterr(**old_settings)
+            self.potential[0, 0, 0] = 0.
+            # Multiply by delta(k).
+            self.potential *= self.plan_c2r.data_in
+        else:
+            self.potential = None
         delta = self.plan_c2r.execute()
         self.delta_field_rms = np.std(delta.flat)
         if self.verbose:
@@ -169,7 +201,28 @@ class Generator(object):
 
         return delta
 
-    def plot_slice(self, num_sections=4, slice_index=0, figure_width=10.,
+    def calculate_newtonian_potential(self,
+                                      show_plot=False, save_plot_name=None):
+        """
+        Calculate the Newtonian potential dPhi(x,y,z) at redshift zero.
+        """
+        if self.potential is None:
+            raise RuntimeError('No saved potential field.')
+        self.plan_c2r.data_in[:] = self.potential
+        rho0 = self.cosmology.critical_density0 * self.cosmology.Om0
+        scale = (-4 * np.pi * astropy.constants.G * rho0).to(u.s**-2).value
+        self.plan_c2r.data_in *= scale
+        field = self.plan_c2r.execute()
+
+        if show_plot or save_plot_name is not None:
+            self.plot_slice(
+                show_plot=show_plot, save_plot_name=save_plot_name,
+                label='Newtonian potential $\Phi(r)$ (Mpc/h)$^2$/s$^2$')
+
+        return field
+
+
+    def plot_slice(self, slice_index=0, figure_width=10.,
                    label='Field values', cmap='jet',
                    clip_percent=1.0, clip_symmetric=False, axis_dz=0.1,
                    field_of_view_deg=3.5, show_plot=False, save_plot_name=None):
@@ -193,14 +246,9 @@ class Generator(object):
         field = self.plan_c2r.data_out
         redshifts = self.redshifts
         zfunc = self.redshift_to_index
+        num_sections = self.num_plot_sections
 
         ny, nz = field.shape[1:3]
-        # Would be better to automate calculation of num_sections here to give
-        # num_sections**2 (ny/nz) ~ 1.
-        if nz % num_sections != 0:
-            raise ValueError(
-                'Z-axis does not evenly divided into {0} sections.'
-                .format(num_sections))
         sections = np.linspace(0, nz, 1 + num_sections, dtype=int)
         x_limits = (0, nz // num_sections - 1)
         y_center = 0.5 * (ny - 1)
